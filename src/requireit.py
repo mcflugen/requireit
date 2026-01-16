199beae51a0db1f1c329a1c768d5659de4d77475
mcflugen <mcflugen@gmail.com>
2026-01-16 10:56:14 -0700

    bump version to v0.2.0

--- src/requireit.py
+++ src/requireit.py
@@ -8,7 +8,7 @@ from numpy.typing import ArrayLike
 from numpy.typing import DTypeLike
 from numpy.typing import NDArray
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 
 class RequireItError(Exception):
